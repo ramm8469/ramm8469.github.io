b6a16a2249285542282ca656e9aa90b1b1d3e1d3
Ram Mohan <rmohan@eseye.com>
2026-01-24 15:16:14 +0530

    revamed and rebuilt the porftolio

--- js/main.js
+++ js/main.js
@@ -103,37 +103,14 @@
 	
 	};
 
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
-
 	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-
+		// Skills animation handled by generic contentWayPoint
 	};
-
 
 	// Loading page
 	var loaderPage = function() {
 		$(".fh5co-loader").fadeOut("slow");
 	};
-
 	
 	$(function(){
 		contentWayPoint();
